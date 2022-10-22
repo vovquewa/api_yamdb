2bd5e650da06d0d4d98162e6df9e09831bc3fcc3
Владимир Козлов <kit.jke@gmail.com>
2022-10-22 14:53:08 +0300

    flake8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -12,7 +12,7 @@ class IsAdmin(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS or (
-                request.user.is_authenticated and (
+            request.user.is_authenticated and (
                 request.user.is_admin
                 or request.user.is_superuser
             ))
