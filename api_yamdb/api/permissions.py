d1a2a78fb513a8039e1849c31d06ce79f5f06565
Юлия Родина <morkovka007.89@mail.ru>
2022-10-22 11:56:35 +0300

    After revew permissions 2

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,9 +11,9 @@ class IsAdmin(permissions.BasePermission):
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_admin
+        return request.method in permissions.SAFE_METHODS or (
+                request.user.is_authenticated and (
+                request.user.is_admin
                 or request.user.is_superuser
             ))
 
